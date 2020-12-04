d0f4f3b7522e9006448318ecac082e5f7f6593f6
panos <panagiotis8panagiotopoulos@gmail.com>
2020-12-04 16:17:52 +0200

    unit tests: remove unused variables

--- src/tests/unit/training/training.service.spec.ts
+++ src/tests/unit/training/training.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { of } from 'rxjs';
 
@@ -13,29 +13,17 @@ const input: Exercise[] = [
 ];
 
 const data = of(input);
-
 let trainingService: TrainingService;
-let httpMock: HttpTestingController;
-let angularFirestore: AngularFirestore;
-
-const collectionSnapshotChangesStub = {
-  snapshotChanges: jasmine.createSpy('snapshotChanges').and.returnValue(data)
-};
-
-const collectionValueChangesStub = {
-  valueChanges: jasmine.createSpy('valueChanges').and.returnValue(data)
-};
-
-const angularFirestoreSnapshotChangesStub = {
-  collection: jasmine.createSpy('collection').and.returnValue(collectionSnapshotChangesStub)
-};
-
-const angularFirestoreValueChangesStub = {
-  collection: jasmine.createSpy('collection').and.returnValue(collectionValueChangesStub)
-};
 
 describe('TrainingService', () => {
   describe('fetch available exercises', () => {
+    // stub snapshotChanges and collection methods of AngularFirestore
+    const collectionSnapshotChangesStub = {
+      snapshotChanges: jasmine.createSpy('snapshotChanges').and.returnValue(data)
+    };
+    const angularFirestoreSnapshotChangesStub = {
+      collection: jasmine.createSpy('collection').and.returnValue(collectionSnapshotChangesStub)
+    };
     beforeEach(() => {
       TestBed.configureTestingModule({
         imports: [
@@ -47,9 +35,7 @@ describe('TrainingService', () => {
         ]
       });
 
-      trainingService = TestBed.get(TrainingService);
-      angularFirestore = TestBed.get(AngularFirestore);
-      httpMock = TestBed.get(HttpTestingController);
+      trainingService = TestBed.inject(TrainingService);
     });
 
     it('should be created', () => {
@@ -63,6 +49,12 @@ describe('TrainingService', () => {
   });
 
   describe('fetch finished/cancelled exercises', () => {
+    const collectionValueChangesStub = {
+      valueChanges: jasmine.createSpy('valueChanges').and.returnValue(data)
+    };
+    const angularFirestoreValueChangesStub = {
+      collection: jasmine.createSpy('collection').and.returnValue(collectionValueChangesStub)
+    };
     beforeEach(() => {
       TestBed.configureTestingModule({
         imports: [
@@ -74,9 +66,7 @@ describe('TrainingService', () => {
         ]
       });
 
-      trainingService = TestBed.get(TrainingService);
-      angularFirestore = TestBed.get(AngularFirestore);
-      httpMock = TestBed.get(HttpTestingController);
+      trainingService = TestBed.inject(TrainingService);
     });
 
     it('should be called with the "finishedExercises" collection', () => {
